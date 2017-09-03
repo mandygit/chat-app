05f52bd5fed28941b96b0a28255456c6781d176d
Mandy <mandeep.nandal89@gmail.com>
2017-09-03 20:44:45 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New User Connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
     
     socket.on('createMessage', (message) => {
         console.log(message);
