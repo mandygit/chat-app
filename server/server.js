c4f93c77ef09a620ea24cde65a7124598cfecac6
Mandy <mandeep.nandal89@gmail.com>
2017-09-10 00:57:52 +0530

    Send messages to people in same room

--- server/server.js
+++ server/server.js
@@ -36,8 +36,10 @@ io.on('connection', (socket) => {
     });
     
     socket.on('createMessage', (message, callback) => {
-        console.log(message);
-        io.emit('newMessage', generateMessage(message.from, message.text))
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)) {
+           io.to(user.room).emit('newMessage', generateMessage(user.name, message.text))   
+        }
         callback();
     });
 
@@ -50,8 +52,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
-    })
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))   
+         }       
+    });
 });
 
 app.use(express.static(publicPath));
